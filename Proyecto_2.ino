d72d9ade357a52a40fca348a9fba299cb6e197ff
Demilon1710 <dele23205@uvg.edu.gt>
2025-05-22 15:54:11 -0600

    Progra final proyecto 2 (sin posiciones exactas)

--- Proyecto_2.ino
+++ Proyecto_2.ino
@@ -1,53 +1,61 @@
-#include <Servo.h>          // Incluye la librería Servo
+#include <Servo.h> //Incluir la librería servo
 
-Servo myservoa;
+Servo myservoa; //Nombrar variable para el servo
 Servo myservob;
 Servo myservoc;
 Servo myservod;
-int potaPin = A0; //Pin al que se conecta el potenciómetro
+int potaPin = A0; //Pin analógico al que se conecta el potenciómetro
 int potbPin = A1;
 int potcPin = A3;
 int potdPin = A5; 
-int pota = 0;
+int pota = 0; //Valores iniciales del contador de cada potenciómetro
 int potb = 0;
 int potc = 0;
-int potd = 0; //Contador del valor en el potenciómetro
-const int botonA = 12;
+int potd = 0; 
+const int botonA = 12; //Pin digital para los botones
 const int botonB = 10;
-const int palanca = 6;
-bool adelante;
+const int palanca = 6; //Pin digital para el interruptor
+const int ledA = 8; //Pin digital para ambos leds
+const int ledB = 7;
+bool adelante; //Declaración de variables booleanas para los botones
 bool atras;
 bool estado;
-int current = 0;
+int current = 0; //Contador para el cambio de estados
 int next = 0;
 
-void moverNangulo(int angulo);
+void moverNangulo(int angulo); //Declaración de funciones propias
+void numerosPot();
 
 const long intervalc = 500; //Variable para el intervalo que indica cuánto tardará el cambio de estado en milisegundos
 unsigned long previousMillis = 0; //Variable para reiniciar el contador de tiempo
 
 void setup() {
-  myservoa.attach(3);
+  myservoa.attach(3); //Configurar la señal que llegará al servomotor desde el pin seleccionado
   myservob.attach(9);
-  myservoc.attach(5);
+  myservoc.attach(5); 
   myservod.attach(11);
 
-  pinMode(botonA, INPUT);
+  pinMode(botonA, INPUT); //Configuración de los botones y el interruptor como entradas pulldown
   pinMode(botonB, INPUT);
   pinMode(palanca, INPUT);
+  pinMode(ledA, OUTPUT); //Configuración de los leds como salidas
+  pinMode(ledB, OUTPUT);
 
-  moverNangulo(0);  
+  moverNangulo (0); //Estado inicial de la función propia.
 
   Serial.begin(9600); //Inicial serialización
 }
 
 void loop() {
-estado = digitalRead(palanca);
-if(estado == HIGH){
-adelante = digitalRead(botonA);
+estado = digitalRead(palanca); //Lectura de la señal proveniente del interruptor
+//If que indica lo que sucederá cuando el interruptor esté en la posición que se conectó para que se considere en valor de 1.
+if (estado == HIGH){
+digitalWrite(ledA, HIGH); 
+digitalWrite(ledB, LOW);
+adelante = digitalRead(botonA); //Declaración de variables para la lectura de los botones
 atras = digitalRead(botonB);
-Serial.println(next);
-Serial.println(current);
+escribePosicion(); //Función para nombrar la posición actual
+  //Instrucción para realizar diferentes acciones de acuerdo al estado actual.
   switch(current){
     //Condiciones de cambio desde el estado 0
     case 0:
@@ -106,7 +114,7 @@ Serial.println(current);
   switch(current){
     //Señales enviadas durante el estado 0
     case 0:
-      moverNangulo(0);
+      moverNangulo(0); //Función original para mover todos los servos de acuerdo al número indicado
     break;
     //Señales enviadas durante el estado 1
     case 1:
@@ -127,18 +135,41 @@ Serial.println(current);
       moverNangulo(180);
     break;
 }
-  unsigned long completeMillis = millis();
+  unsigned long completeMillis = millis(); //Instrucción millis para indicar el tiempo que se tardan en actuar los botones
   if (completeMillis - previousMillis >= intervalc) {
     current = next;
     previousMillis = completeMillis;
   }
 }
-if(estado == LOW){
+//If con las instrucciones cuando el interruptor esté en estado 0.
+if (estado == LOW){
+  digitalWrite(ledA, LOW); //Indicar el estado de salida que se busca para los leds
+  digitalWrite(ledB, HIGH);
   pota = analogRead(potaPin); //El contador cambiará de acuerdo a la lectura del potenciómetro en el pin A3
   potb = analogRead(potbPin);
   potc = analogRead(potcPin);
   potd = analogRead(potdPin);
-  estado = digitalRead(palanca);
+  int potenciasa = map(pota, 0, 1023, 0, 180); //Mapeo desde 0 a 1024 para 0 a 180
+  int potenciasb = map(potb, 0, 1023, 0, 180);
+  int potenciasc = map(potc, 0, 1023, 0, 180);
+  int potenciasd = map(potd, 0, 1023, 0, 180);
+  numerosPot();
+  myservoa.write(potenciasa); //Desde el pin del servo, se mandará el valor mapeado del potenciómetro
+  myservob.write(potenciasb);
+  myservoc.write(potenciasc);
+  myservod.write(potenciasd);
+}
+delay(1);
+}
+//Contenido de las funciones originales
+void moverNangulo(int angulo){
+  myservoa.write(angulo);
+  myservob.write(angulo);
+  myservoc.write(angulo);
+  myservod.write(angulo);
+}
+
+void numerosPot(){
   int potenciasa = map(pota, 0, 1023, 0, 180); //Mapeo desde 0 a 1024 para 0 a 180
   int potenciasb = map(potb, 0, 1023, 0, 180);
   int potenciasc = map(potc, 0, 1023, 0, 180);
@@ -151,17 +182,9 @@ if(estado == LOW){
   Serial.println(potenciasc);
   Serial.println("Potenciómetro D: ");
   Serial.println(potenciasd);
-  myservoa.write(potenciasa); //Desde el pin del servo, se mandará el valor mapeado del potenciómetro
-  myservob.write(potenciasb);
-  myservoc.write(potenciasc);
-  myservod.write(potenciasd);
-}
-delay(1);
 }
 
-void moverNangulo(int angulo){
-  myservoa.write(angulo);
-  myservob.write(angulo);
-  myservoc.write(angulo);
-  myservod.write(angulo);
+void escribePosicion(){
+  Serial.println(next);
+  Serial.println(current);
 }
